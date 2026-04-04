84866eb7d46b00bc9da835a29c65e4be8fc3d257
lrhi199507-lang <lrhi199507@gmail.com>
2026-04-03 21:39:38 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Configuración optimizada para generar la APK
 export default defineConfig({
   plugins: [react()],
   build: {
