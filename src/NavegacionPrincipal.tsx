3924e041b7cb9ca42b8704873c909e70965c26a5
lrhi199507-lang <lrhi199507@gmail.com>
2026-04-11 11:12:34 -0400

    Update NavegacionPrincipal.tsx

--- src/NavegacionPrincipal.tsx
+++ src/NavegacionPrincipal.tsx
@@ -1,4 +1,4 @@
-Import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { auth, db } from "./firebaseConfig";
 import { signOut } from "firebase/auth";
 import {
