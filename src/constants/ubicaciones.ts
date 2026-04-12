38b23c649f77ed8321b3722cc9f5770a98c7703a
lrhi199507-lang <lrhi199507@gmail.com>
2026-04-12 18:11:39 -0400

    Create ubicaciones.ts

---
+++ src/constants/ubicaciones.ts
@@ -0,0 +1,23 @@
+// --- CONSTANTES DE UBICACIÓN ---
+const UBICACIONES = {
+  "Amazonas": ["Puerto Ayacucho"], 
+  "Anzoátegui": ["Barcelona", "Puerto La Cruz"],
+  "Apure": ["San Fernando"], 
+  "Aragua": ["Maracay", "Turmero", "La Victoria"],
+  "Barinas": ["Barinas"], 
+  "Bolívar": ["Ciudad Guayana", "Ciudad Bolívar"],
+  "Carabobo": ["Valencia", "Naguanagua", "Guacara", "San Diego"],
+  "Cojedes": ["San Carlos", "Tinaquillo"], 
+  "Distrito Capital": ["Caracas"],
+  "Falcón": ["Coro", "Punto Fijo"], 
+  "Lara": ["Barquisimeto", "Cabudare"],
+  "Mérida": ["Mérida", "El Vigía"], 
+  "Miranda": ["Los Teques", "Chacao", "Baruta"],
+  "Monagas": ["Maturín"], 
+  "Nueva Esparta": ["Porlamar"], 
+  "Portuguesa": ["Guanare"],
+  "Táchira": ["San Cristóbal"], 
+  "Trujillo": ["Valera"], 
+  "Yaracuy": ["San Felipe"],
+  "Zulia": ["Maracaibo", "San Francisco"]
+};
