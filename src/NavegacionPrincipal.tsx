846c65d1975026b16475cd952365d823fb5f9e7a
lrhi199507-lang <lrhi199507@gmail.com>
2026-04-26 11:20:49 -0400

    Update NavegacionPrincipal.tsx

--- src/NavegacionPrincipal.tsx
+++ src/NavegacionPrincipal.tsx
@@ -38,13 +38,31 @@ const [viajeForm, setViajeForm] = useState({
 
   // Base de datos de ubicaciones para el autocompletado
   const UBICACIONES = {
-    "Carabobo": ["Valencia", "Naguanagua", "San Diego", "Guacara", "Puerto Cabello"],
-    "Distrito Capital": ["Caracas"],
-    "Aragua": ["Maracay", "Turmero", "La Victoria"],
-    "Lara": ["Barquisimeto", "Cabudare"],
-    "Zulia": ["Maracaibo"]
-  };
-
+      "Amazonas": ["Puerto Ayacucho", "Puerto Páez"], 
+  "Anzoátegui": ["Barcelona", "Puerto La Cruz", "El Tigre", "Anaco"],
+  "Apure": ["San Fernando", "Guasdualito"], 
+  "Aragua": ["Maracay", "Turmero", "La Victoria", "Cagua"],
+  "Barinas": ["Barinas", "Socopó"], 
+  "Bolívar": ["Ciudad Guayana", "Ciudad Bolívar", "Upata", "Santa Elena de Uairén"],
+  "Carabobo": ["Valencia", "Naguanagua", "Guacara", "San Diego", "Puerto Cabello", "Mariara", "Los Guayos"],
+  "Cojedes": ["San Carlos", "Tinaquillo"], 
+  "Delta Amacuro": ["Tucupita"],
+  "Distrito Capital": ["Caracas"],
+  "Falcón": ["Coro", "Punto Fijo", "Tucacas", "Chichiriviche"], 
+  "Guárico": ["San Juan de los Morros", "Calabozo", "Valle de la Pascua"],
+  "Lara": ["Barquisimeto", "Cabudare", "Carora", "El Tocuyo"],
+  "La Guaira": ["La Guaira", "Maiquetía", "Catia La Mar"],
+  "Mérida": ["Mérida", "El Vigía", "Tovar"], 
+  "Miranda": ["Los Teques", "Chacao", "Baruta", "Guatire", "Guarenas", "Charallave", "Higuerote"], 
+  "Monagas": ["Maturín", "Punta de Mata"], 
+  "Nueva Esparta": ["Porlamar", "Pampatar", "Juan Griego"], 
+  "Portuguesa": ["Guanare", "Acarigua", "Araure"],
+  "Sucre": ["Cumaná", "Carúpano"],
+  "Táchira": ["San Cristóbal", "La Grita", "San Antonio del Táchira"], 
+  "Trujillo": ["Trujillo", "Valera", "Boconó"], 
+  "Yaracuy": ["San Felipe", "Yaritagua", "Chivacoa"],
+  "Zulia": ["Maracaibo", "San Francisco", "Cabimas", "Ciudad Ojeda"]
+};
   // Función para enviar a Firebase (Colección "Viajes")
   const publicarRuta = async (datosFinales) => {
     try {
