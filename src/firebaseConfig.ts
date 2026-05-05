bd5234927a0a2a17d34082be1c4a0fc3b74d1c7c
lrhi199507-lang <lrhi199507@gmail.com>
2026-05-05 19:54:16 -0400

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // <-- AGREGAR ESTA LÍNEA
+import { getStorage } from "firebase/storage";
+import { getMessaging } from "firebase/messaging"; // <-- Importación para notificaciones
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXBs3-Z4-SC2UUAtZhjMgMZ74sD9rqq9Y",
@@ -16,7 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app); // <-- INICIALIZAR STORAGE
+const storage = getStorage(app);
+const messaging = getMessaging(app); // <-- Inicializar mensajería
 
-// EXPORTAMOS TODO
-export { auth, db, storage }; // <-- AGREGAR storage AQUÍ
+export { auth, db, storage, messaging }; // <-- Exportar todo junto
