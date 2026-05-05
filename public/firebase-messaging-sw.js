4c94e0cc059ac46a9a11c15d2163b6af81103bbc
lrhi199507-lang <lrhi199507@gmail.com>
2026-05-05 19:55:36 -0400

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,30 +1,24 @@
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
-// ⚠️ REEMPLAZA ESTO CON TUS DATOS REALES DE FIREBASE
 const firebaseConfig = {
-  apiKey: "BLa_YfTJFTTFpYubl3GEZOy6Wo6cL0fBmG9z0-n2v6wA7V08nwQI7oK5RKPFIDF1QjW4RUxsdMqTIHDiEatFrOk",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyCXBs3-Z4-SC2UUAtZhjMgMZ74sD9rqq9Y", // La clave correcta
+  authDomain: "rutacom-4ea87.firebaseapp.com",
+  projectId: "rutacom-4ea87",
+  storageBucket: "rutacom-4ea87.firebasestorage.app",
+  messagingSenderId: "1080063705561",
+  appId: "1:1080063705561:web:13da1c6fe35eb0e40ad9c8"
 };
 
-// Inicializamos la app en segundo plano
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Este código detecta la notificación cuando la app está cerrada o en segundo plano
 messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Mensaje recibido en background ', payload);
-  
+  console.log('Mensaje recibido en segundo plano:', payload);
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/favicon.svg', // Icono que aparecerá en la notificación
-    badge: '/favicon.svg', // Icono pequeño
-    vibrate: [200, 100, 200, 100, 200, 100, 200] // Patrón de vibración
+    icon: '/favicon.svg' 
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
