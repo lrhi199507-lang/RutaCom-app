a4c52e0d4e0c1652bdbb62273f48820cf67a2d3e
lrhi199507-lang <lrhi199507@gmail.com>
2026-05-25 14:25:05 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rutacom.app',
-  appName: 'RutaCom',
-  webDir: 'dist', // CRÍTICO: Debe ser 'dist'
+  appId: 'com.damelacola.app', // <-- Pon este texto exacto en la casilla de Google Play
+  appName: 'Dame la cola',     // <-- Nombre oficial
+  webDir: 'dist', 
   server: {
     androidScheme: 'https'
   }
