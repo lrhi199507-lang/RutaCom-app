9098eef364dcfd0bf4919201688d47db80ffe645
lrhi199507-lang <lrhi199507@gmail.com>
2026-06-06 19:14:43 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { VerificadorActualizacion } from "./components/VerificadorActualizacion";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <VerificadorActualizacion />
+    <App />
+  </>
+);
